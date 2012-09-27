c95e64fae77747f0a643a74a3671f5ae1c517de0
Vyacheslav Enis <venis@difane.com>
2012-09-27 12:37:10 +0300

    Initial commit of DfnSymfonyDistribution adopted to git repository.

---
+++ app/features/admin-contentpart.feature
@@ -0,0 +1,37 @@
+Feature: Content Part administration
+   In order to change textual content on website
+   As website administrator
+   I need to be able to manage content parts
+   
+   Scenario: Accessing content part management
+      Given I am logged in as "VEnis" with "123qwe" password
+      When I follow "ContentPart"
+      Then I should see "Content Part List"
+
+   Scenario: Creating, editing and deleting new content part
+      Given I am logged in as "VEnis" with "123qwe" password
+      When I follow "ContentPart"
+      And follow "Add new"
+      Then I should see "Content Part Create"
+      When I fill in the following:
+         | Name              | MyTestContentPart  |
+         | Title             | Test content part  |
+         | Raw Content       | * My List Item     |
+         | Content Formatter | markdown           |
+      And I press "Create"
+      Then I should see "Item has been successfully created."
+      And I should see "Edit \"MyTestContentPart\""
+      
+      When I follow "Return to list"
+      Then I should see "Content Part List"
+      And I should see "MyTestContentPart"
+
+      When I follow "MyTestContentPart"
+      And I should see "Edit \"MyTestContentPart\""
+
+      When I follow "Delete"
+      Then I should see "Content Part Delete"
+      And I should see "Confirm deletion"
+      When I press "Yes, delete"
+      Then I should see "Item has been deleted successfully."
+      And I should see "Content Part List"
---
+++ src/Behat/Context/DistributionContext.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Behat\Context;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+use Behat\Symfony2Extension\Context\KernelAwareInterface;
+use Behat\MinkExtension\Context\MinkContext;
+
+use Behat\Behat\Context\BehatContext,
+    Behat\Behat\Context\Step,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Feature context.
+ */
+class DistributionContext extends MinkContext //MinkContext if you want to test web
+                  implements KernelAwareInterface
+{
+    private $kernel;
+    private $parameters;
+
+    /**
+     * Initializes context with parameters from behat.yml.
+     *
+     * @param array $parameters
+     */
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Sets HttpKernel instance.
+     * This method will be automatically called by Symfony2Extension ContextInitializer.
+     *
+     * @param KernelInterface $kernel
+     */
+    public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @Given /^I am logged in as "([^"]*)" with "([^"]*)" password$/
+     */
+    public function iAmLoggedInAsWithPassword($username, $password)
+    {
+        return array(
+            new Step\Given("I am on \"/admin/logout\""),
+            new Step\When("I go to \"/admin/dashboard\""),
+            new Step\When("I fill in \"username\" with \"$username\""),
+            new Step\When("I fill in \"password\" with \"$password\""),
+            new Step\When("I press \"Login\""),
+            new Step\Then("I should be on \"/admin/dashboard\""),
+        );
+    }
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        $container = $this->kernel->getContainer();
+//        $container->get('some_service')->doSomethingWith($argument);
+//    }
+//
+}
